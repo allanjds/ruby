bf2669cccc787030e18b9c094028e019ea7b2a43
Allan Jose <48626596+allanjds@users.noreply.github.com>
2020-06-11 02:23:30 -0300

    cenario saldo insuficiente
    Quando o cliente tentar sacar 100,00 com saldo 0,00

--- unitarios/spec/saque_spec.rb
+++ unitarios/spec/saque_spec.rb
@@ -1,13 +1,18 @@
 
 class ContaCorrente
-    attr_accessor :saldo
-    
+    attr_accessor :saldo, :mensagem
+
+     
     def initialize(saldo)
         self.saldo = saldo
     end
 
     def saca(valor)
-        self.saldo -= valor
+        if (valor > self.saldo)
+            self.mensagem = 'Saldo insuficiente para saque :('
+        else
+            self.saldo -= valor
+        end
     end
 
 end
@@ -22,6 +27,19 @@ describe ContaCorrente do
             end
             it 'entao atualiza saldo' do
                 expect(@conta.saldo).to eql 800.00
+            end
+        end
+
+        context 'quando o saldo é insuficiente' do
+            before(:all) do
+                @conta = ContaCorrente.new(0.00)
+                @conta.saca(100)
+            end
+            it 'vejo mensagem' do
+                expect(@conta.mensagem).to eql 'Saldo insuficiente para saque :('
+            end
+            it 'meu saldo permanececom zeros' do
+                expect(@conta.saldo).to eql 0.00
             end
         end
     end
